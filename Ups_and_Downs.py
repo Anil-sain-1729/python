ddeaa8119fe9b234dc2115b511c1d85b9a013eb2
Anil_sain_1729 <asain4773@gmail.com>
2026-03-16 19:48:59 +0530

    ups and downs

---
+++ Ups_and_Downs.py
@@ -0,0 +1,15 @@
+# cook your dish here
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    a = list(map(int,input().split()))
+    a.sort()
+    for i in range(n-1):
+        if(i%2==0):
+            if(a[i]>a[i+1]):
+                a[i],a[i+1] = a[i+1],a[i]
+        else:
+            if(a[i]<a[i+1]):
+                a[i],a[i+1] = a[i+1],a[i]
+                
+    print(a)
